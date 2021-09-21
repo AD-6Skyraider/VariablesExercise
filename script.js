cbd081048e3c36bc3ad54a7062701a7abec51719
Don Vest <tsukasasan@hotmail.com>
2021-09-21 15:28:38 -0500

    Created and completed Variables Exercise

---
+++ script.js
@@ -0,0 +1,43 @@
+// 1
+// const - A variable type whose variable will not change.
+
+// 2
+let num = 20;
+
+// 3
+num = 15;
+console.log(num);
+
+// 4
+num = 15+7;
+console.log(num);
+
+// 5
+num--;
+console.log(num);
+
+// 6
+num = "This is not a number";
+console.log(num);
+
+// 7
+let blank ="";
+
+// 8
+console.log(blank);
+
+// 9
+blank =null;
+console.log(blank);
+
+// Bonus 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+
+// Bonus 2
+console.log(quarter, dime, nickel, penny);
+
+// Bonus 3
+let letters = "qwertyuiop";
+
+// Bonus 4
+console.log("letters".length, [5]);
